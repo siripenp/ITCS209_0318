2c434dda54b7a3d68c1dbabb9287208e3497737a
siripenp <siripen.pon@mahidol.edu>
2019-06-22 18:29:14 +0700

    Add First Project

---
+++ FirstProject/src/FirstJava.java
@@ -0,0 +1,50 @@
+public class FirstJava {
+
+	public static void main(String[] args) {
+		// Ex. 1
+		System.out.println("Hello World!");
+		
+		// Self-Check
+		System.out.println("Hello");
+		System.out.println("World!");
+		
+		// Self-Check
+		System.out.print("My lucky number is "); 
+		System.out.println(3 + 4 + 5); 
+		
+		// Self-Check
+		System.out.println("My lucky number is " + 3 + 4 + 5); 
+		
+		// Task 1
+		System.out.println("In Java, a character preceded by a backslash (\\) is an escape sequence.");
+		System.out.println("\\n is to inseart a new line and \\\" is to insert a do.outuble quote in the text.");
+		System.out.println("For example, I said \"I love Java\".");
+		
+		// Task 2 (1)
+		System.out.println("XXXXXXXXX XXXXXXXXXX");
+		System.out.println("XXXXXXXXX XXXXXXXXXX");
+		System.out.println("XX        XX      XX");
+		System.out.println("XX        XX      XX");
+		System.out.println("XXXXXXXXX XXXXXXXXXX");
+		System.out.println("XXXXXXXXX XXXXXXXXXX");
+		System.out.println("       XX XX        ");
+		System.out.println("       XX XX        ");
+		System.out.println("XXXXXXXXX XX        ");
+		System.out.println("XXXXXXXXX XX        ");
+		
+		// Task 2 (2)
+		System.out.println("XXXXXXXXXXXXXXXXXXXXXXX");
+		System.out.println("XX        XX         XX");
+		System.out.println("XX XXXXXXXXX XXXXXXX XX");
+		System.out.println("XX XXXXXXXXX XXXXXXX XX");
+		System.out.println("XX XXXXXXXXX XXXXXXX XX");
+		System.out.println("XX        XX         XX");
+		System.out.println("XXXXXXXXX XX XXXXXXXXXX");
+		System.out.println("XXXXXXXXX XX XXXXXXXXXX");
+		System.out.println("XXXXXXXXX XX XXXXXXXXXX");
+		System.out.println("XX        XX XXXXXXXXXX");
+		System.out.println("XXXXXXXXXXXXXXXXXXXXXXX");
+		
+	}
+
+}
